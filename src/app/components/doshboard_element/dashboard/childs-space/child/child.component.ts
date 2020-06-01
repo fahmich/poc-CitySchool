51c0569ad35a80600abfa3a4783d64a0246f8bcb
fahmichibani <fahmichibani@gmail.com>
2020-06-01 06:30:41 +0100

    fix css fils

--- src/app/components/doshboard_element/dashboard/childs-space/child/child.component.ts
+++ src/app/components/doshboard_element/dashboard/childs-space/child/child.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { child } from 'src/app/Shared/models/child';
 
 @Component({
   selector: 'app-child',
@@ -7,27 +9,74 @@ import { Component, Input, OnInit } from "@angular/core";
 })
 export class ChildComponent implements OnInit {
   @Input() data;
-  workedDays = [{ day: "", morning: "", afterNoon: "" }]
-  constructor() {
-    this.workedDays = [
-      { day: "Lundi", morning: "1", afterNoon: "2" },
-      { day: "Mardi", morning: "3", afterNoon: "4'" },
-      { day: "Mercredi", morning: "5", afterNoon: "6" },
-      { day: "Jeudi", morning: "7", afterNoon: "8" },
-      { day: "Vendredi", morning: "9", afterNoon: "10" },
-      { day: "Samedi", morning: "11", afterNoon: "12" },
-      { day: "Dimanche", morning: "13", afterNoon: "14" },
-    ]
-   }
+  userForm: FormGroup;
+  child: child = new child();
+
+   constructor(
+    private formBuilder: FormBuilder,
+
+  ) {
+      
+  }
    selected = 'option2';
 
 
   ngOnInit(): void {
+    this.userForm = new FormGroup({});
+    this.userForm = this.createUserModelForm();
+
+  }
+  createUserModelForm() {
+    return this.formBuilder.group({
+      Anglais: [this.child.Anglais],
+      AnglaisEnLinge: [this.child.AnglaisEnLinge],
+      Anglaisch: [this.child.Anglaisch],
+      Anglaisformat: [this.child.Anglaisformat],
+      Français: [this.child.Français],
+      FrançaisEnLinge: [this.child.FrançaisEnLinge],
+      Françaisformat: [this.child.Françaisformat],
+      Math: [this.child.Math],
+      Physiquechime: [this.child.Physiquechime],
+      checkboxfr: [this.child.checkboxfr],
+      date: [this.child.date],
+      format: [this.child.format],
+      formuleEnLinge: [this.child.formuleEnLinge],
+      formulepresentiel: [this.child.formulepresentiel],
+      genre: [this.child.genre],
+      // name: [this.child.name],
+      nom: [this.child.nom],
+      physquechimie: [this.child.physquechimie],
+      physquechimieEnLinge: [this.child.physquechimieEnLinge],
+      physquechimieformat: [this.child.physquechimieformat],
+      prenon: [this.child.prenon],
+      scolaire: [this.child.scolaire],
+      Lundi   : [this.child.Lundi],
+      Lundi1  : [this.child.Lundi1],
+      Mardi  : [this.child.Mardi],
+      Mardi1 : [this.child.Mardi1],
+       Mercredi : [this.child.Mercredi],
+       Mercredi1 : [this.child.Mercredi1],
+       Jeudi : [this.child.Jeudi],
+       Jeudi1: [this.child.Jeudi1],
+       Vendredi : [this.child.Vendredi],
+       Vendredi1 : [this.child.Vendredi],
+       Samedi  : [this.child.Samedi],
+       Samedi1 : [this.child.Samedi],
+       Dimanche : [this.child.Dimanche],
+       Dimanche1 : [this.child.Dimanche1],
+
+     
+
+    
+    });
   }
   opencard1:boolean=false
   opencard2:boolean=false
   opencard3:boolean=false
   opencard4:boolean=false
+  onSubmit(UserForm) {
+    console.log(UserForm.value)
+  }
 
 
   open(id){
